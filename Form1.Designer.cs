ec7642848daf423b5facc3df07b087a800aae799
jrcarver <jrcman13@gmail.com>
2022-11-26 20:15:48 -0600

    All buttons on GUI implemented.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,12 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.show_games = new System.Windows.Forms.Button();
             this.show_customers = new System.Windows.Forms.Button();
+            this.show_employees = new System.Windows.Forms.Button();
+            this.show_sales = new System.Windows.Forms.Button();
+            this.games_join_sales = new System.Windows.Forms.Button();
+            this.customers_join_sales = new System.Windows.Forms.Button();
+            this.employees_join_sales = new System.Windows.Forms.Button();
+            this.join_all = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,10 +55,10 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(368, 63);
+            this.dataGridView1.Location = new System.Drawing.Point(269, 63);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(420, 375);
+            this.dataGridView1.Size = new System.Drawing.Size(519, 375);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -60,27 +66,93 @@
             // 
             this.show_games.Location = new System.Drawing.Point(12, 63);
             this.show_games.Name = "show_games";
-            this.show_games.Size = new System.Drawing.Size(169, 49);
+            this.show_games.Size = new System.Drawing.Size(113, 42);
             this.show_games.TabIndex = 2;
             this.show_games.Text = "Show Games";
             this.show_games.UseVisualStyleBackColor = true;
-            this.show_games.Click += new System.EventHandler(this.button1_Click);
+            this.show_games.Click += new System.EventHandler(this.show_games_Click);
             // 
             // show_customers
             // 
-            this.show_customers.Location = new System.Drawing.Point(187, 63);
+            this.show_customers.Location = new System.Drawing.Point(131, 63);
             this.show_customers.Name = "show_customers";
-            this.show_customers.Size = new System.Drawing.Size(175, 49);
+            this.show_customers.Size = new System.Drawing.Size(113, 42);
             this.show_customers.TabIndex = 3;
             this.show_customers.Text = "Show Customers";
             this.show_customers.UseVisualStyleBackColor = true;
-            this.show_customers.Click += new System.EventHandler(this.button2_Click);
+            this.show_customers.Click += new System.EventHandler(this.show_customers_Click);
+            // 
+            // show_employees
+            // 
+            this.show_employees.Location = new System.Drawing.Point(12, 111);
+            this.show_employees.Name = "show_employees";
+            this.show_employees.Size = new System.Drawing.Size(113, 42);
+            this.show_employees.TabIndex = 4;
+            this.show_employees.Text = "Show Employees";
+            this.show_employees.UseVisualStyleBackColor = true;
+            this.show_employees.Click += new System.EventHandler(this.show_employees_Click);
+            // 
+            // show_sales
+            // 
+            this.show_sales.Location = new System.Drawing.Point(131, 111);
+            this.show_sales.Name = "show_sales";
+            this.show_sales.Size = new System.Drawing.Size(113, 42);
+            this.show_sales.TabIndex = 5;
+            this.show_sales.Text = "Show Sales";
+            this.show_sales.UseVisualStyleBackColor = true;
+            this.show_sales.Click += new System.EventHandler(this.show_sales_Click);
+            // 
+            // games_join_sales
+            // 
+            this.games_join_sales.Location = new System.Drawing.Point(12, 159);
+            this.games_join_sales.Name = "games_join_sales";
+            this.games_join_sales.Size = new System.Drawing.Size(113, 42);
+            this.games_join_sales.TabIndex = 6;
+            this.games_join_sales.Text = "Games join Sales";
+            this.games_join_sales.UseVisualStyleBackColor = true;
+            this.games_join_sales.Click += new System.EventHandler(this.games_join_sales_Click);
+            // 
+            // customers_join_sales
+            // 
+            this.customers_join_sales.Location = new System.Drawing.Point(131, 159);
+            this.customers_join_sales.Name = "customers_join_sales";
+            this.customers_join_sales.Size = new System.Drawing.Size(113, 42);
+            this.customers_join_sales.TabIndex = 7;
+            this.customers_join_sales.Text = "Customers join Sales";
+            this.customers_join_sales.UseVisualStyleBackColor = true;
+            this.customers_join_sales.Click += new System.EventHandler(this.customers_join_sales_Click);
+            // 
+            // employees_join_sales
+            // 
+            this.employees_join_sales.Location = new System.Drawing.Point(12, 207);
+            this.employees_join_sales.Name = "employees_join_sales";
+            this.employees_join_sales.Size = new System.Drawing.Size(113, 42);
+            this.employees_join_sales.TabIndex = 8;
+            this.employees_join_sales.Text = "Employees join Sales";
+            this.employees_join_sales.UseVisualStyleBackColor = true;
+            this.employees_join_sales.Click += new System.EventHandler(this.employees_join_sales_Click);
+            // 
+            // join_all
+            // 
+            this.join_all.Location = new System.Drawing.Point(131, 207);
+            this.join_all.Name = "join_all";
+            this.join_all.Size = new System.Drawing.Size(113, 42);
+            this.join_all.TabIndex = 9;
+            this.join_all.Text = "Join all tables";
+            this.join_all.UseVisualStyleBackColor = true;
+            this.join_all.Click += new System.EventHandler(this.join_all_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.join_all);
+            this.Controls.Add(this.employees_join_sales);
+            this.Controls.Add(this.customers_join_sales);
+            this.Controls.Add(this.games_join_sales);
+            this.Controls.Add(this.show_sales);
+            this.Controls.Add(this.show_employees);
             this.Controls.Add(this.show_customers);
             this.Controls.Add(this.show_games);
             this.Controls.Add(this.dataGridView1);
@@ -100,5 +172,11 @@
         private DataGridView dataGridView1;
         private Button show_games;
         private Button show_customers;
+        private Button show_employees;
+        private Button show_sales;
+        private Button games_join_sales;
+        private Button customers_join_sales;
+        private Button employees_join_sales;
+        private Button join_all;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,8 @@
 using System.Data;
 using System.Data.SQLite;
+using System.IO;
 using System.Security.AccessControl;
+using System.Windows.Forms;
 
 namespace databases_project
 {
@@ -8,10 +10,6 @@ namespace databases_project
     {
         // database path
         string path = "BoardGameStore.db";
-
-        SQLiteConnection con;
-        SQLiteCommand cmd;
-        SQLiteDataReader dr;
 
         public Form1()
         {
@@ -23,8 +21,13 @@ namespace databases_project
 
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
         // show Games table data
-        private void button1_Click(object sender, EventArgs e)
+        private void show_games_Click(object sender, EventArgs e)
         {
             //connection object
             SQLiteConnection con = new SQLiteConnection(@"data source=" + path);
@@ -42,14 +45,130 @@ namespace databases_project
             dataGridView1.DataSource = dt;
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void show_customers_Click(object sender, EventArgs e)
         {
-            
+            //connection object
+            SQLiteConnection con = new SQLiteConnection(@"data source=" + path);
+
+            // command object
+            string query = "SELECT * FROM Customers";
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            // datatable
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            // insert data into dataGrid
+            dataGridView1.DataSource = dt;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void show_sales_Click(object sender, EventArgs e)
         {
-            
+            //connection object
+            SQLiteConnection con = new SQLiteConnection(@"data source=" + path);
+
+            // command object
+            string query = "SELECT * FROM Sales";
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            // datatable
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            // insert data into dataGrid
+            dataGridView1.DataSource = dt;
+        }
+
+        private void show_employees_Click(object sender, EventArgs e)
+        {
+            //connection object
+            SQLiteConnection con = new SQLiteConnection(@"data source=" + path);
+
+            // command object
+            string query = "SELECT * FROM Employees";
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            // datatable
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            // insert data into dataGrid
+            dataGridView1.DataSource = dt;
+        }
+
+        private void games_join_sales_Click(object sender, EventArgs e)
+        {
+            //connection object
+            SQLiteConnection con = new SQLiteConnection(@"data source=" + path);
+
+            // command object
+            string query = "SELECT S.ID AS saleID, G.ID AS gameID, G.game, G.price, S.custEmail, S.empEmail, S.date FROM Games G INNER JOIN Sales S ON G.ID = S.gameID";
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            // datatable
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            // insert data into dataGrid
+            dataGridView1.DataSource = dt;
+        }
+
+        private void customers_join_sales_Click(object sender, EventArgs e)
+        {
+            //connection object
+            SQLiteConnection con = new SQLiteConnection(@"data source=" + path);
+
+            // command object
+            string query = "SELECT S.ID, C.email AS custEmail, C.lastName AS custLast, C.firstName AS custFirst, C.phone AS custPhone, S.empEmail, S.date FROM Customers C INNER JOIN Sales S ON C.email = S.custEmail";
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            // datatable
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            // insert data into dataGrid
+            dataGridView1.DataSource = dt;
+        }
+
+        private void employees_join_sales_Click(object sender, EventArgs e)
+        {
+            //connection object
+            SQLiteConnection con = new SQLiteConnection(@"data source=" + path);
+
+            // command object
+            string query = "SELECT S.ID, S.gameID, S.custEmail, S.empEmail, E.lastName AS empLast, E.firstName AS empFirst, E.phone AS empPhone, S.date FROM Employees E INNER JOIN Sales S ON E.Email = S.empEmail";
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            // datatable
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            // insert data into dataGrid
+            dataGridView1.DataSource = dt;
+        }
+
+        private void join_all_Click(object sender, EventArgs e)
+        {
+            //connection object
+            SQLiteConnection con = new SQLiteConnection(@"data source=" + path);
+
+            // command object
+            string query = "SELECT S.ID AS saleID, G.game, G.price, S.custEmail, C.lastName AS custLast, C.firstName AS custFirst, C.phone AS custPhone, S.empEmail, E.lastName AS empLast, E.firstName AS empFirst, E.phone AS empPhone, S.date FROM Games G INNER JOIN Sales S ON G.ID = S.gameID INNER JOIN Customers C ON C.email = S.custEmail INNER JOIN Employees E ON E.email = S.empEmail";
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+
+            // datatable
+            DataTable dt = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            // insert data into dataGrid
+            dataGridView1.DataSource = dt;
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
